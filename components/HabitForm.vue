d7432558e7d1b71d5ba8426be7d16c33d578be4e
EafenQAQ <eafenqaq@gmail.com>
2025-09-22 16:53:48 +0800

    feat: 创建表单和UI基本样式

--- components/HabitForm.vue
+++ components/HabitForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form @submit.prevent="handleSubmit" class="mb-6 flex items-center gap-2">
+    <form class="mb-6 flex items-center gap-2" @submit.prevent="handleSubmit">
         <input v-model="name" type="text" placeholder="Enter a new habit..."
             class="w-full p-3 border border-gray-300 rounded-sm" />
         <button type="submit" class="w-full p-3 bg-purple-500 text-white rounded-sm">
